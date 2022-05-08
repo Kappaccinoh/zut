4b8541670fdc028cc84d11a9e46d9f50a44ffc86
Kappaccinoh <lim.jia28@gmail.com>
2022-05-08 23:15:05 +0800

    models and tables set up

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,8 @@
 class Room < ApplicationRecord
     validates_uniqueness_of :name
     has_many :messages
+    has_many :groupparticipants
+    belongs_to :user
     
     scope :public_rooms, -> { where(is_private: false) }
     after_create_commit { broadcast_append_to "rooms" }
