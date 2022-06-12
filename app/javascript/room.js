0526827e6977530ebb10ec6c97c37f16bf4298a8
Kappaccinoh <lim.jia28@gmail.com>
2022-06-12 18:54:09 +0800

    minor changes stable include jquery2

--- app/javascript/room.js
+++ app/javascript/room.js
@@ -1,8 +1,11 @@
 /* Contains all the self written Javascript Related to Homepage and in Specific Rooms */
 //= require jquery
+//= require jquery2
 
 $(document).on('ready', function() {
     console.log("Javascripts Loaded")
 
     $('#category-modal').modal()
+
+    console.log($('#category-modal'))
 })
